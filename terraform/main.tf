a0964dcac30741ae2141bf964d9e8ca758d008b3
Vlad A Chiriac <vladc.@vlads-mbp.home>
2024-03-23 11:06:43 +0000

    Add variables file and imaged folder for diagram

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
-    region = "eu-west-2"
+    region = var.aws_region
+    profile = "default"
 }
 
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
