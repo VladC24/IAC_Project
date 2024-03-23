8afb3198f694fdf33ba55206827b3295e672e97c
Vlad A Chiriac <vladc.@vlads-mbp.home>
2024-03-23 19:11:31 +0000

    Update eks module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+module "eks" {
+    source                    = "./modules/eks"
+    aws_public_subnet         = module.vpc.aws_public_subnet
+    vpc_id                    = module.vpc.vpc_id
+    cluster_name              = "module-eks-${random_string.suffix.result}"
+    endpoint_public_access    = true
+    endpoint_private_access   = false
+    public_access_cidrs       = ["0.0.0.0/0"]
+    node_group_name           = "iacproject"
+    scaling_desired_size      = 1
+    scaling_max_size          = 1
+    scaling_min_size          = 1
+    instance_type             = ["t2.micro"]
+    key_pair                  = "KeyPair"
+}
+
 module "vpc" {
     source                   = "./modules/vpc"
     tags                     = "iacproject"
@@ -9,3 +25,4 @@ module "vpc" {
     map_public_ip_on_launch = true 
     rt_route_cidr_block      = "0.0.0.0/0"
 }
+
